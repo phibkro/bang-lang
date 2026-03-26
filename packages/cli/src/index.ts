608c0b151f35364879d8470c4605b61a4c6c3a2e
phibkro <71797726+phibkro@users.noreply.github.com>
2026-03-26 15:43:07 +0100

    feat(cli): add bang fmt command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env bun
 import { Args, Command } from "@effect/cli";
+import { FileSystem } from "@effect/platform";
 import { BunContext, BunRuntime } from "@effect/platform-bun";
+import { Formatter } from "@bang/core";
 import { Effect } from "effect";
 import { compileFile } from "./Compile.js";
 import { runFile } from "./Run.js";
@@ -15,7 +17,19 @@ const run = Command.make("run", { filePath }).pipe(
   Command.withHandler(({ filePath }) => runFile(filePath)),
 );
 
-const bang = Command.make("bang").pipe(Command.withSubcommands([compile, run]));
+const fmtCmd = Command.make("fmt", { filePath }).pipe(
+  Command.withHandler(({ filePath }) =>
+    Effect.gen(function* () {
+      const fs = yield* FileSystem.FileSystem;
+      const source = yield* fs.readFileString(filePath);
+      const formatted = yield* Formatter.formatSource(source);
+      yield* fs.writeFileString(filePath, formatted);
+      yield* Effect.log(`Formatted ${filePath}`);
+    }),
+  ),
+);
+
+const bang = Command.make("bang").pipe(Command.withSubcommands([compile, run, fmtCmd]));
 
 const cli = Command.run(bang, { name: "bang", version: "0.0.1" });
 
